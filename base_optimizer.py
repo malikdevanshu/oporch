de539034eef00f0fae886f09c64a2ac9b87a8c5c
malikdevanshu <malikdevanshu1507@gmail.com>
2026-09-05 22:52:22 +0200

    seperating sgd from the optimizer

--- base_optimizer.py
+++ base_optimizer.py
@@ -92,6 +92,62 @@ class BaseOptimizer(ABC):
             model.w2 -= alpha * gradients["w2"]
             model.b2 -= alpha * gradients["b2"]
 
+    def optimize(self, model, X, y):
+            n = y.shape[1]
+            cost = Loss()
+    
+            for iteration in range(self.max_iter):
+    
+                y_hat, cache = model.forward_pass(X)
+    
+                loss = cost.multiclass_cross_entropy(y_hat, y, n)#-(1 / n) * np.sum(y * np.log(y_hat))
+    
+                gradients = model.backward(
+                    cache, 
+                    y,
+                )
+    
+                grad_norm = self.gradient_norm_squared(
+                    gradients
+                )
+    
+                if np.sqrt(grad_norm) < self.tolerance:
+                    break
+    
+                print("iteration:", iteration)
+                print("loss before step:", loss)
+                print("gradient norm:", np.sqrt(grad_norm))
+    
+    
+                alpha = self.line_search(
+                    model,
+                    X,
+                    y,
+                    gradients,
+                    loss,
+                )
+                print("alpha:", alpha)
+    
+                self.update(
+                    model,
+                    gradients,
+                    alpha,
+                )
+    
+                new_y_hat, _ = model.forward_pass(X)
+    
+                new_loss = -(1 / n) * np.sum(
+                    y * np.log(new_y_hat)
+                )
+    
+                print("loss after step:", new_loss)
+                print("-------------------")
+    
+                self.history.append(new_loss)
+    
+            return self.history
+    
+
     def mini_batch(self, X, y, batch_size, shuffle):
             n_samples = X.shape[0]
             indices = np.arange(n_samples)
--- optimizer.py
+++ optimizer.py
@@ -3,60 +3,27 @@ from base_optimizer import BaseOptimizer
 from losses import Loss
 
 class SGD(BaseOptimizer):
-    def optimize(self, model, X, y):
-        n = y.shape[1]
-        cost = Loss()
+    def __init__(self):
+        super().__init__()
 
-        for iteration in range(self.max_iter):
+    def update(
+            self,
+            model,
+            gradients,
+            alpha,
+        ):
+        model.w1 -= alpha * gradients["w1"]
+        model.b1 -= alpha * gradients["b1"]
 
-            y_hat, cache = model.forward_pass(X)
+        model.w2 -= alpha * gradients["w2"]
+        model.b2 -= alpha * gradients["b2"]
 
-            loss = cost.multiclass_cross_entropy(y_hat, y, n)#-(1 / n) * np.sum(y * np.log(y_hat))
-
-            gradients = model.backward(
-                cache, 
-                y,
-            )
-
-            grad_norm = self.gradient_norm_squared(
-                gradients
-            )
-
-            if np.sqrt(grad_norm) < self.tolerance:
-                break
-
-            print("iteration:", iteration)
-            print("loss before step:", loss)
-            print("gradient norm:", np.sqrt(grad_norm))
+        
 
 
-            alpha = self.line_search(
-                model,
-                X,
-                y,
-                gradients,
-                loss,
-            )
-            print("alpha:", alpha)
 
-            self.step(
-                model,
-                gradients,
-                alpha,
-            )
 
-            new_y_hat, _ = model.forward_pass(X)
 
-            new_loss = -(1 / n) * np.sum(
-                y * np.log(new_y_hat)
-            )
-
-            print("loss after step:", new_loss)
-            print("-------------------")
-
-            self.history.append(new_loss)
-
-        return self.history
 
     
                 
